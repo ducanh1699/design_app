b4c1fb755864ba2ee17221855af2bd593f5983d1
ducanh1699 <ducanh1699@gmail.com>
2022-09-12 23:44:05 +0700

    main add core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(new MyApp());
+// void main() {
+//   runApp(new MyApp());
+// }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
