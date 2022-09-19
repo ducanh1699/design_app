5b2c9a9c4854ce3f60169ea60ae410674c260f8f
ducanh1699 <ducanh1699@gmail.com>
2022-09-20 00:00:58 +0700

    ducanh firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:firebase1/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(new MyApp());
-// }
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Flutter login demo',
-        theme: new ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: new LoginPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const WidgetTree(),
+    );
   }
 }
